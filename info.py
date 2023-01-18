c730852241dcd1d3db399c033aaba6e079e30063
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2023-01-18 22:03:40 +0400

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
     
       # URL Shortener #
 
-URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'api.shareus.in/shortLink')
+URL_SHORTENR_WEBSITE = environ.get('URL_SHORTENR_WEBSITE', 'Clicksfly.com')
 URL_SHORTNER_WEBSITE_API = environ.get('URL_SHORTNER_WEBSITE_API', 'spI2WWtNqRYs6Sp6yW5BXdLgoRu2')
 
      # Auto Delete For Group Message (Self Delete) #
