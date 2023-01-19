c6922a1878f80f9c87fa808374c09ff491931110
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2023-01-19 05:10:01 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -383,7 +383,7 @@ async def get_shortlink(link):
         https = "https"
         link = link.replace("http", https)
 
-    url = f'https://dulink.in/api'
+    url = f'https://clicksfly.com/api'
     params = {'token': URL_SHORTNER_WEBSITE_API,
               'link': link,
               'format': 'json'
